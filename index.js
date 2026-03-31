984a11340f99de3770dc6714134cf2c192bdbe40
Josuemr <Josuemr@etehort.ps.sp>
2026-03-30 22:48:23 -0300

    20260330 - com rotas

--- index.js
+++ index.js
@@ -7,26 +7,33 @@ const app = express();
 
 // configurações iniciais
 app.use(morgan("dev"));
+app.set("views", "./views"); //adicionado
+app.set("view engine", "ejs"); //adicionado
 
 // rotas da aplicação
 app.get("/", (req, res) => {
-  res.send("rota raiz");
+  //res.send("rota raiz");
+  res.render("index",{mensagem:"rota raiz", titulo:"pagina principal"})
 });
 
 app.get("/qualquercoisa", (req, res) => {
-  res.send("rota qualquer coisa");
+  //res.send("rota qualquer coisa");
+  res.render("index",{mensagem:"Rota qualquer coisa", titulo:"qualquer coisa"})
 });
 
 app.get("/contato", (req, res) => {
-  res.send("rota contato");
+  //res.send("rota contato");
+  res.render("index",{mensagem:"rota contato", titulo:"contato"})
 });
 
 app.get("/sobre", (req, res) => {
-  res.send("rota sobre");
+  //res.send("rota sobre");
+  res.render("index",{mensagem:"rota sobre", titulo:"sobre"})
 });
 
 app.use((req, res) => {
-  res.status(404).send("Página não encontrada!");
+  //res.status(404).send("Página não encontrada!");
+  res.status(404).render("erro404",{titulo:"erro 404"})
 });
 
 // Coloca o servidor no ar
